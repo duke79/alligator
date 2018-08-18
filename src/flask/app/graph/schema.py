ac2c767157809acc9a4a96950e0b2e6a2e72c9ab
duke79 <pulkitsingh01@gmail.com>
2018-08-18 21:56:09 +0530

    flask | user | categories

--- src/flask/app/graph/schema.py
+++ src/flask/app/graph/schema.py
@@ -8,11 +8,13 @@ from app.data import db
 from app.data.permissions import UserPermission
 from app.graph.category import Category, CategoriesAction
 from app.graph.channel import Channel, ChannelsAction
+from app.graph.user import User
 
 
 class Query(graphene.ObjectType):
-    channels = graphene.List(Channel, inputs=graphene.Argument(ChannelsAction, required=False, default_value={}))
-    categories = graphene.List(Category, inputs=graphene.Argument(CategoriesAction, required=False, default_value={}))
+    channels = graphene.List(Channel, action=graphene.Argument(ChannelsAction, required=False, default_value={}))
+    categories = graphene.List(Category, action=graphene.Argument(CategoriesAction, required=False, default_value={}))
+    current_user = graphene.List(User)
 
     def resolve_channels(self, info):
         channels = db.parse_all_channels()
@@ -20,6 +22,9 @@ class Query(graphene.ObjectType):
 
     def resolve_categories(self, info, inputs):
         return [{"title": "asd"}]
+
+    def resolve_current_user(self, info):
+        return {}
 
 
 schema = graphene.Schema(query=Query)
--- src/flask/app/graph/user.py
+++ src/flask/app/graph/user.py
@@ -2,10 +2,37 @@ import graphene
 from app.data import db
 
 
+class ActionAddUserCategories(graphene.InputObjectType):
+    id = graphene.List(graphene.Int)
+
+
+class ActionRemoveUserCategories(graphene.InputObjectType):
+    id = graphene.List(graphene.Int)
+
+
+class ActionUpdateUserCategories(graphene.InputObjectType):
+    id = graphene.List(graphene.Int)
+
+
+class ActionGetUserCategories(graphene.InputObjectType):
+    ids = graphene.List(graphene.Int, description="List of category ids to limit this action to.")
+    query = graphene.String(description="To search categories by name.")
+    channel_id = graphene.Int()
+    limit = graphene.Int(default_value=10)
+
+
+class UserCategoriesAction(graphene.InputObjectType):
+    add = graphene.Field(ActionAddUserCategories)
+    update = graphene.Field(ActionUpdateUserCategories)
+    remove = graphene.Field(ActionRemoveUserCategories)
+    get = graphene.Field(ActionGetUserCategories)
+
+
 class User(graphene.ObjectType):
     """User class for graph"""
     name = graphene.String()
     email = graphene.String()
+    categories = graphene.List("app.graph.category.Category", action=graphene.Argument(UserCategoriesAction, required=False, default_value={}))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
