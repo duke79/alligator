c057c384f24398445dacacc123ba62e24ee93395
duke79 <pulkitsingh01@gmail.com>
2018-08-24 22:36:20 +0530

    sqlalchemy | table | category

--- src/flask/scripts/create_db_schema.py
+++ src/flask/scripts/create_db_schema.py
@@ -1,5 +1,6 @@
 from app.graph.user.alchemy import User
 from app.graph.channel.alchemy import Channel
+from app.graph.category.alchemy import Category
 from app import db
 
 if __name__ == "__main__":
