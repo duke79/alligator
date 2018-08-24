ad605575d85b6c363e3fd343a336aa1a439d787c
duke79 <pulkitsingh01@gmail.com>
2018-08-24 20:47:19 +0530

    sqlalchemy | .

--- src/flask/app/__init__.py
+++ src/flask/app/__init__.py
@@ -1,7 +1,20 @@
 # HTTP Error Codes and their meaning: https://www.symantec.com/connect/articles/http-error-codes
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.data.config import Config
 
+""" App """
+## Init app
 app = Flask(__name__)
+## Init SQLAlchemy
+config = Config()["database"]["mysql"]
+db_uri = "mysql+pymysql://{0}:{1}@{2}:3306/{3}" \
+    .format(config["user"], config["password"], config["host"], config["db"])
+# db_uri = "sqlite:///sqlalchemy_example.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+db = SQLAlchemy(app)
+# db.create_all() # No use until the model classes are imported. Use scripts.create_db_schema instead.
 
+""" Views """
 from app.views import graph  # /graph
---
+++ src/flask/scripts/create_db_schema.py
@@ -0,0 +1,5 @@
+from app.graph.user.alchemy import User
+from app import db
+
+if __name__ == "__main__":
+    db.create_all()
